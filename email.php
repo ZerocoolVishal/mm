1f580df778eeb28ad9e14634b6cc577212bddf48
Vishal Bhosle <vishal@Vishals-MacBook-Air.local>
2019-09-30 17:48:13 +0530

    new changes

--- email.php
+++ email.php
@@ -1,7 +1,46 @@
 <?php
 
+$servername = "localhost";
+$username = "mm_user";
+$password = "Mind@123456";
+$dbname = "mindmechanics";
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$mobile = $_GET['mobile'];
+$subject = $_GET['subject'];
+$message = $_GET['message'];
+
 $from = "anupama.mind@gmail.com";
 $to = "vishal.devtaa@gmail.com";
-$message = "Hello";
+$email_subject = "Message from Mind Mechanics";
+
+$body = "
+    Name: $name \n
+    Email: $email \n
+    Mobile: $mobile \n
+    Subject: $subject \n
+    Message: $message \n
+";
 $headers = "From: appointment@mindmechanics.in";
-mail($to, "My subject", $message, $headers);
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO users (name, email, mobile, subject, message)
+    VALUES ('John', 'Doe', 'john@example.com')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+}
+catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+if(mail($to, $email_subject, $body, $headers)) {
+    header('location: /thankyou.html');
+}
+else {
+    echo "Email Not send";
+}
